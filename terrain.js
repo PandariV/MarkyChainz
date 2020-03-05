08e8d2e2e82e1cfcb22ff8d2d27a7e045141a62a
PandariV <43098305+PandariV@users.noreply.github.com>
2020-03-05 14:16:34 -0600

    make the slope variable plz

--- terrain.js
+++ terrain.js
@@ -3,12 +3,12 @@ class Terrain {
         this.w = w;
         this.h = h;
         this.s = s;
-        this.body = Matter.Bodies.trapezoid(x, y, w, h, s)
+        this.body = Matter.Bodies.trapezoid(x, y, w, h, s, {isStatic: true});
         World.add(world, this.body);
     }
 
     show() {
-        fill(255);
+        fill(58, 201, 63);
 
         push();
         translate(this.body.position.x, this.body.position.y);
