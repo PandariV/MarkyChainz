acce32e0d26578a222c33c586197070480643269
PandariV <43098305+PandariV@users.noreply.github.com>
2020-03-04 18:51:54 -0600

    finished the car

--- script.js
+++ script.js
@@ -5,21 +5,57 @@ var terrainMap = [];
 
 var mill = 0;
 
+var font, txtSpd;
+
+function preload() {
+    font = loadFont("Roboto-Light.ttf");
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     engine = Engine.create();
     Engine.run(engine);
 
     world = engine.world;
-    ground = Bodies.rectangle(width*100/2, height, width * 100, 40, {isStatic: true});
+    ground = Bodies.rectangle(width*5/2 - 50, height, width * 5, 40, {isStatic: true});
     World.add(world, ground);
 
     car = new Car(1);
+    txtSpd = car.chasis.speed;
+}
 
+function draw() {
+    background(49, 219, 222);
+    noStroke();
+    rectMode(CENTER);
+    textFont(font);
+    textSize(32);
+    fill(255);
+    text("Speed: " + txtSpd, 10, 32);
+
+    translate(-car.chasis.position.x + car.w + 100, 0);
+
+    //car
+    car.show();
+    if(millis() - mill >= 500) {
+        txtSpd = nf(car.chasis.speed, 2, 2);
+        car.update();
+        console.log(car.chasis.speed);
+        mill = millis();
+    }
+
+    //ground
+    fill(0, 255, 0);
+    rect(ground.position.x, ground.position.y, width*5, 40);
+
+    //obstacles
+    for(var t of terrainMap) {
+        t.show();
+    }
 }
 
 function mousePressed() {
-    terrainMap.push(new Terrain(mouseX, mouseY, 50, 50));
+    terrainMap.push(new Terrain(mouseX + car.chasis.position.x - car.w - 100, mouseY, 50, 50));
 }
 
 function keyPressed() {
@@ -27,27 +63,4 @@ function keyPressed() {
         Composite.remove(world, t.body);
     }
     terrainMap = [];
-}
-
-function draw() {
-    background(49, 219, 222);
-    noStroke();
-    rectMode(CENTER);
-
-    //car
-    car.show();
-    if(millis() - mill >= 500) {
-        car.update();
-        console.log(car.chasis.speed);
-        mill = millis();
-    }
-    
-    //ground
-    fill(0, 255, 0);
-    rect(ground.position.x, ground.position.y, width, 40);
-
-    //obstacles
-    for(var t of terrainMap) {
-        t.show();
-    }
 }
